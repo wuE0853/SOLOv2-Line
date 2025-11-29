2eba4df209de16ba13f8008bfd4f47415278931e
wue <2235947920@qq.com>
2025-11-29 11:37:30 +0800

    Complete utils.utils.ConvModule

--- utils/utils.py
+++ utils/utils.py
@@ -130,4 +130,159 @@ def load_state_dict(module, state_dict, strict=False):
             print(err_msg)
     
 
+def bias_init_with_prob(prior_prob):
+    # initialize conv/fc bias value according to giving prior probability
+    bias_init = float(-np.log(1 - prior_prob) / prior_prob)
+    return bias_init
 
+
+def build_conv_layer(*args, **kwargs):
+    return nn.Conv2d(*args, **kwargs)
+
+
+norm_cfg = {'BN': ('bn', nn.BatchNorm2d),
+            'SyncBN': ('bn', nn.SyncBatchNorm),
+            'GN': ('gn', nn.GroupNorm)}
+
+def build_norm_layer(cfg, num_features, postfix=''):
+    """ Build normalization layer
+
+    Args:
+        cfg (dict): cfg should contain:
+            type (str): identify norm layer type.
+            layer args: args needed to instantiate a norm layer.
+            requires_grad (bool): [optional] whether stop gradient updates
+        num_features (int): number of channels from input.
+        postfix (int, str): appended into norm abbreviation to
+            create named layer.
+
+    Returns:
+        name (str): abbreviation + postfix
+        layer (nn.Module): created norm layer
+    """
+    assert isinstance(cfg, dict) and 'type' in cfg, 'The config of norm layer has wrong type'
+    cfg_ = cfg.copy()
+
+    layer_type = cfg_.pop('type')
+    if layer_type not in norm_cfg:
+        raise KeyError('Unrecognized norm type{}'.format(layer_type))
+    else:
+        abbr, norm_layer = norm_cfg[layer_type]
+        if norm_layer is None:
+            raise NotImplementedError
+    
+    assert isinstance(postfix, (int, str)), 'The postfix of norm layer should be int or str'
+    name = abbr + str(postfix)
+
+    requires_grad = cfg_.pop('requires_grad', True)
+    cfg_.setdefault('eps', 1e-5)
+    if layer_type != 'GN':
+        layer = norm_layer(num_features, **cfg_)
+        if layer_type == 'SyncBN':
+            layer._specify_ddp_gpu_num(1)
+        else:
+            assert ' num_groups' in cfg_, 'num_groups is necessary in GN'
+            layer = norm_layer(num_channels=num_features, **cfg_)
+        
+        for param in layer.parameters():
+            param.requires_grad = requires_grad
+        
+        return name, layer
+
+
+class ConvModule(nn.module):
+    """A conv block that contains conv/norm/activation layers.
+
+    Args:
+        in_channels (int): Same as nn.Conv2d.
+        out_channels (int): Same as nn.Conv2d.
+        kernel_size (int or tuple[int]): Same as nn.Conv2d.
+        stride (int or tuple[int]): Same as nn.Conv2d.
+        padding (int or tuple[int]): Same as nn.Conv2d.
+        dilation (int or tuple[int]): Same as nn.Conv2d.
+        groups (int): Same as nn.Conv2d.
+        bias (bool or str): If specified as `auto`, it will be decided by the
+            norm_cfg. Bias will be set as True if norm_cfg is None, otherwise False.
+        conv_cfg (dict): Config dict for convolution layer.
+        norm_cfg (dict): Config dict for normalization layer.
+        activation (str or None): Activation type, "ReLU" by default.
+        inplace (bool): Whether to use inplace mode for activation.
+        order (tuple[str]): The order of conv/norm/activation layers. It is a
+            sequence of "conv", "norm" and "act". Examples are
+            ("conv", "norm", "act") and ("act", "conv", "norm").
+    """
+    def _init_(self,
+               in_channels,
+               out_channel,
+               kernel_size,
+               stride=1,
+               padding=0,
+               dilation=1,
+               groups=1,
+               bias='auto',
+               norm_cfg=None,
+               activation='relu',
+               inplace=True,
+               order=('conv', 'norm', 'act')):
+        super(ConvModule, self)._init_()
+        assert norm_cfg is None or isinstance(norm_cfg, dict), 'norm_cfg has wrong type'
+        self.activation = activation
+        self.inplace = inplace
+        self.order = order
+        assert isinstance(self.order, tuple) and len(self.order) == 3, 'The conv order is wrong'
+        assert set(order) == set(['conv', 'act', 'norm']), 'Unrecognized layer name in ConvModule'
+
+        self.with_norm = norm_cfg is not None
+        self.with_activation = activation is not None
+        # if the conv layer is before a norm layer, bias is unnecessary
+        if bias == 'auto':
+            bias = False if self.with_norm else True
+        self.with_bias = bias
+
+        if self.with_norm and self.with_bias:
+            warnings('ConvModule has norm and bias at the same time.')
+
+        #build convolution layer
+        self.conv = build_conv_layer(in_channels,
+                                     out_channel,
+                                     kernel_size,
+                                     stride=stride,
+                                     padding=padding,
+                                     dilation=dilation,
+                                     groups=groups,
+                                     bias=bias)
+        
+        #export the attributes of self.conv to a higher level for convenience
+        self.in_channels = self.conv.in_channels
+        self.out_channels = self.conv.out_channels
+        self.kernel_size = self.conv.kernel_size
+        self.stride = self.conv.stride
+        self.padding = self.conv.padding
+        self.dilation = self.conv.dilation
+        self.transposed = self.conv.transposed
+        self.output_padding = self.conv.output_padding
+        self.groups = self.conv.groups
+
+        if self.with_norm:
+            self.gn = nn.GroupNorm(num_groups=32, num_channels=self.out_channels)
+        if self.with_activation:
+            self.activate = nn.ReLU(inplace=inplace)
+
+        # Use msra init by default
+        self.init_weights()
+
+    def init_weights(self):
+        nonlinearity = 'relu' if self.activation is None else self.activation
+        kaiming_init(self.conv, nonlinearity=nonlinearity)
+        if self.with_norm:
+            constant_init(self.gn, 1, bias=0)
+    
+    def forward(self, x):
+        for layer in self.order:
+            if layer == 'conv':
+                x = self.conv(x)
+            elif layer == 'norm' and self.with_norm:
+                x = self.gn(x)
+            elif layer == 'act' and self.with_activation:
+                x = self.activate(x)
+        return x
