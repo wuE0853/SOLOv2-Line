af2ca45b1e512610e830f92fc25c776786984f23
wue <2235947920@qq.com>
2025-11-30 16:35:13 +0800

    Complete utils.utils(matrix_nms)

--- utils/utils.py
+++ utils/utils.py
@@ -286,3 +286,54 @@ class ConvModule(nn.module):
             elif layer == 'act' and self.with_activation:
                 x = self.activate(x)
         return x
+    
+
+def matrix_nms(seg_mask, cate_labels, cate_scores, sigma: float = 2.0, sum_masks=None):
+    """Matrix NMS for multi-class masks.
+
+    Args:
+        seg_masks (Tensor): shape (n, h, w)
+        cate_labels (Tensor): shape (n), mask labels in descending order
+        cate_scores (Tensor): shape (n), mask scores in descending order
+        sigma (float): std in gaussian method
+        sum_masks (Tensor): The sum of seg_masks
+
+    Returns:
+        Tensor: cate_scores_update, tensors of shape (n)
+    """
+
+    n_samples = cate_labels.shape[0]
+    if sum_masks is None:
+        sum_masks = seg_mask.sum((1,2)).float()
+    seg_mask = seg_mask.reshape(n_samples, -1).float()
+    # inter
+    inter_matrix = torch.mm(seg_mask, seg_mask.transpose(1, 0))
+    # union.
+    sum_masks_x = sum_masks.expand(n_samples, n_samples)
+    # iou.
+    iou_matrix = (inter_matrix / (sum_masks_x + sum_masks_x.transpose(1, 0) - inter_matrix)).triu(diagonal=1)
+    # label_specific matrix.
+    cate_labels_x = cate_labels.expand(n_samples, n_samples)
+    label_matrix = (cate_labels_x == cate_labels_x.transpose(1, 0)).float().triu(diagonal=1)
+
+    # IoU compensation
+    compensate_iou, _ = (iou_matrix * label_matrix).max(0)
+    compensate_iou = compensate_iou.expand(n_samples, n_samples).transpose(1, 0)
+
+    # IoU decay
+    decay_iou = iou_matrix * label_matrix
+
+    # matrix nms, kernel == 'gaussian'
+    decay_matrix = torch.exp(-1 * sigma * (decay_iou ** 2))
+    compensate_matrix = torch.exp(-1 * sigma * (compensate_iou ** 2))
+    decay_coefficient, _ = (decay_matrix / compensate_matrix).min(0)
+
+    # update the score.
+    cate_scores_update = cate_scores * decay_coefficient
+    return cate_scores_update
+
+
+def multi_apply(func, *args, **kwargs):
+    pfunc = partial(func, **kwargs) if kwargs else func
+    map_results = map(pfunc, *args)
+    return tuple(map(list, zip(*map_results)))    
